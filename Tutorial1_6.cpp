b78b38d388b09a4c23dd93b2d20997a0a441650b
Madhav Nandan <mnandan06@gmail.com>
2015-02-22 00:19:24 +0530

    modification in Tut1 , lect.6

---
+++ Tutorial1_6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Here is some text \n";
+    cout << "Here is a number: ";
+    cout << 10;
+    cout << endl;
+
+    cout << "How much is 15 + 17? the answer is:";
+    cout << 15 + 17;
+
+    cin.get();
+    return 0;
+}
---
+++ stdafx.h
@@ -0,0 +1,21 @@
+// stfafx.h : include file 
+// or project specific include file
+// are changed infrequently
+//
+
+
+#pragma once
+
+
+#include "targetver.h"
+
+
+#include<stdio.h>
+#include<tchar.h>
+
+
+
+
+
+
+// TODO: reference additional headers your file will need
