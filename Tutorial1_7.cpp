29a6ef30e0a9e19fc76789769f11d6c794633513
Madhav Nandan <mnandan06@gmail.com>
2015-02-22 00:37:29 +0530

    modified Tut1 lect7

---
+++ Tutorial1_7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x;
+    int y;
+    x = 7;
+    y = 10;
+    x = 8;
+
+    cout << x ;
+    cout << y ;
+    cout << x + y ;
+
+    cin.get();
+    return 0;
+
+
+}
