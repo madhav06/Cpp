45e6b10cffa27bd1e8c7c2e61e1d8056c4496f08
Madhav Nandan <mnandan06@gmail.com>
2015-02-22 09:54:58 +0530

    signed and unsigned int Tut1 , lect. 8

---
+++ Tutorial1_8.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x;
+    unsigned short int y;
+    unsigned long int z;
+    signed short int a;
+    signed long int b;
+
+    y = 10;
+    z = 1000000;
+    a = -100;
+    b = -1000000;
+
+    cout << y << endl;
+    cout << z << endl;
+    cout << a << endl;
+    cout << b << endl;
+
+    cin.get();
+    return 0;
+}
